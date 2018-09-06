6d8c1e0d2b01944115e7be226fa3b7b3b79de4ef
Luke Pulverenti <luke.pulverenti@gmail.com>
2018-09-06 15:05:51 -0400

    fix thumb format

--- MediaBrowser.Channels.TouTv/Plugin.cs
+++ MediaBrowser.Channels.TouTv/Plugin.cs
@@ -45,14 +45,14 @@ namespace MediaBrowser.Channels.TouTv
         public Stream GetThumbImage()
         {
             var type = GetType();
-            return type.Assembly.GetManifestResourceStream(type.Namespace + ".thumb.png");
+            return type.Assembly.GetManifestResourceStream(type.Namespace + ".thumb.jpg");
         }
 
         public ImageFormat ThumbImageFormat
         {
             get
             {
-                return ImageFormat.Png;
+                return ImageFormat.Jpg;
             }
         }
 
